eb5853e46a3a1079a2b320bb5284fdf657c34c98
Laiane Karla <lannekarla03@gmail.com>
2022-06-08 13:09:05 -0300

    Exercícios membros estáticos

---
+++ src/application/ProgramCurrencyConverter.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
+uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe CurrencyConverter
+para ser responsável pelos cálculos.
+ * 
+ */
+
+public class ProgramCurrencyConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
